936ef17a2a9148a984261ef8f0ca0d9e293bf03b
tripmates24.in <tripmates24.in@gmail.com>
2026-01-20 15:20:29 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,22 +1,21 @@
-import React, { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 interface LayoutProps {
-    children: ReactNode;
-    showFooter?: boolean;
+  children: ReactNode;
+  showFooter?: boolean;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, showFooter = true }) => {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <main className="flex-grow">
-                {children}
-            </main>
-            {showFooter && <Footer />}
-        </div>
-    );
+const Layout = ({ children, showFooter = true }: LayoutProps) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      {showFooter && <Footer />}
+    </div>
+  );
 };
 
 export default Layout;
+
